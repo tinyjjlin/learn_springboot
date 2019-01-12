4cd49d1bdaaf3e939451aff0bb32afd951d2ad22
j100057 <tinyjjlin@163.com>
2019-01-12 20:11:25 +0800

    添加工作流

---
+++ spring-boot-flowable/src/main/java/com/tiny/springbootflowable/SpringBootFlowableApplication.java
@@ -0,0 +1,14 @@
+package com.tiny.springbootflowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootFlowableApplication.class, args);
+    }
+
+}
+
